fb3baca93004007a98f15302e43b5515e04cf0bb
Ram <rammohanofficial03@gmail.com>
2025-01-02 23:09:50 +0530

    Postgres and Prisma

--- Lectures/week-10/week-10-1/src/index.ts
+++ Lectures/week-10/week-10-1/src/index.ts
@@ -85,4 +85,89 @@ async function getUser(email: string) {
     }
 }
 
-getUser("user1@google.com").catch(console.error)
+// getUser("user1@google.com").catch(console.error)
+
+// Write a function to enter both user and address details 
+async function insertUserAndAddress(
+    username: string, 
+    email: string, 
+    password: string, 
+    city: string, 
+    country: string, 
+    street: string, 
+    pincode: string
+) {
+    const client = new Client({
+        connectionString: "postgresql://neondb_owner:rcx45jXiYUpn@ep-cool-smoke-a1gdjyft.ap-southeast-1.aws.neon.tech/neondb?sslmode=require"
+    })
+
+    try {
+        await client.connect()
+
+        await client.query("BEGIN")
+
+        const userQuery = "INSERT INTO Users(username, email, password) VALUES ($1, $2, $3) RETURNING id;"
+        const userValues = [username, email, password]
+        const userRes = await client.query(userQuery, userValues)
+        const userId = userRes.rows[0].id
+
+        const addressQuery = "INSERT INTO Address(user_id, city, country, street, pincode) VALUES ($1, $2, $3, $4, $5)"
+        const addressValues = [userId, city, country, street, pincode]
+        await client.query(addressQuery, addressValues)
+
+        await client.query("COMMIT")
+
+        console.log("User and Address added succesfully!");
+    } catch (err) {
+        await client.query("ROLLBACK")
+        console.error('Error during transaction, rolled back.', err);
+        throw err;
+    } finally {
+        await client.end(); // Close the client connection
+    }
+}
+
+// insertUserAndAddress(
+//     'hopkins', 
+//     'hopkins@example.com', 
+//     '12345678', 
+//     'New York', 
+//     'USA', 
+//     '123 Broadway St', 
+//     '10001'
+// );
+
+// Write a function to fetch both a users details and their address.
+async function getUserandAddress(email: string) {
+    const client = new Client({
+        connectionString: "postgresql://neondb_owner:rcx45jXiYUpn@ep-cool-smoke-a1gdjyft.ap-southeast-1.aws.neon.tech/neondb?sslmode=require"
+    })
+
+    try {
+        await client.connect()
+
+        const query = `
+            SELECT u.id, u.username, u.email, a.city, a.country, a.street, a.pincode
+            FROM Users u 
+            JOIN Address a ON u.id = a.user_id
+            WHERE email = $1;
+        `
+        const res = await client.query(query, [email])
+
+        if(res.rows.length > 0) {
+            console.log("User and address found!", res.rows[0]);
+            return res.rows[0]
+        } 
+        else {
+            console.log("No user with the above email found!");
+            return null
+        }
+    } catch (err) {
+        console.log("Error fetching user with the above email!", err);
+        throw err
+    } finally {
+        await client.end()
+    }
+}
+
+getUserandAddress("user1@google.com")
